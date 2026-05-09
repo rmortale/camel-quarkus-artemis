eaa2f6d475cb2455975aa2103fd8667bef410837
rmortale <rmortale@dulce.ch>
2026-05-09 17:01:36 +0200

    added logging

--- src/main/java/ch/dulce/camel/Templates.java
+++ src/main/java/ch/dulce/camel/Templates.java
@@ -1,5 +1,6 @@
 package ch.dulce.camel;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -10,12 +11,16 @@ public class Templates extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 
-    routeTemplate("upercaseTransformation")
+    routeTemplate("uppercaseTransformation")
         .templateParameter("inqueue")
         .templateParameter("outqueue")
         .templateParameter("maxConsumers", "5")
-        .from("jms:{{inqueue}}?maxConcurrentConsumers={{maxConsumers}}&lazyCreateTransactionManager=false&transacted=true&cacheLevelName=CACHE_CONSUMER")
-        .log("Hello ${body}")
+        .from("""
+            jms:{{inqueue}}?maxConcurrentConsumers={{maxConsumers}}
+            &lazyCreateTransactionManager=false
+            &transacted=true
+            &cacheLevelName=CACHE_CONSUMER""")
+        .log(LoggingLevel.DEBUG, "uppercaseTransformation", "${body}")
         .transform().simple("${uppercase()}")
         .to("jms:{{outqueue}}");
   }
