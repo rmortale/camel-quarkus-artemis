4d1412207ebb560ffd2bf941c4f672c3ef1ed8cd
rmortale <rmortale@dulce.ch>
2026-05-10 09:32:13 +0200

    added wrap body transformation

--- src/main/java/ch/dulce/camel/Templates.java
+++ src/main/java/ch/dulce/camel/Templates.java
@@ -1,15 +1,38 @@
 package ch.dulce.camel;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-
-import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class Templates extends RouteBuilder {
 
+  static final String MESSAGE = """
+      <object>
+        <header>
+          <system>${header.system}</system>
+          <serviceid>${header.serviceid}</serviceid>
+        </header>
+        <body>${body}</body>
+      </object>
+      """;
+
   @Override
   public void configure() throws Exception {
+
+    routeTemplate("wrapbodyTransformation")
+        .templateParameter("inqueue")
+        .templateParameter("outqueue")
+        .templateParameter("maxConsumers", "5")
+        .from("""
+            jms:{{inqueue}}?maxConcurrentConsumers={{maxConsumers}}
+            &lazyCreateTransactionManager=false
+            &transacted=true
+            &cacheLevelName=CACHE_CONSUMER""")
+        .transform().simple(MESSAGE)
+        .log(LoggingLevel.DEBUG, "wrapbodyTransformation", "${body}")
+        .to("jms:{{outqueue}}");
+
 
     routeTemplate("uppercaseTransformation")
         .templateParameter("inqueue")
@@ -20,8 +43,8 @@ public class Templates extends RouteBuilder {
             &lazyCreateTransactionManager=false
             &transacted=true
             &cacheLevelName=CACHE_CONSUMER""")
-        .log(LoggingLevel.DEBUG, "uppercaseTransformation", "${body}")
         .transform().simple("${uppercase()}")
+        .log(LoggingLevel.DEBUG, "uppercaseTransformation", "${body}")
         .to("jms:{{outqueue}}");
   }
 
