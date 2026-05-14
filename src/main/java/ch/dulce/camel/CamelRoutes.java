9e8467af5ccc0d865b657dbc6796a381ce0f2f74
rmortale <rmortale@dulce.ch>
2026-05-14 09:46:19 +0200

    changed to EndpointRouteBuilder

--- src/main/java/ch/dulce/camel/CamelRoutes.java
+++ src/main/java/ch/dulce/camel/CamelRoutes.java
@@ -1,14 +1,22 @@
 package ch.dulce.camel;
 
-import org.apache.camel.builder.RouteBuilder;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 
 @ApplicationScoped
-public class CamelRoutes extends RouteBuilder {
+public class CamelRoutes extends EndpointRouteBuilder {
 
   @Override
   public void configure() throws Exception {
+
+//    from(jms("inqueue")
+//        .transacted(true)
+//        .cacheLevelName("CACHE_CONSUMER")
+//        .maxConcurrentConsumers(5)
+//        .advanced().lazyCreateTransactionManager(false))
+//        .log("${body}");
+
 
     // templatedRoute("extractBodyTransformation")
     // .parameter("inqueue", "extractin")
--- src/main/java/ch/dulce/camel/Templates.java
+++ src/main/java/ch/dulce/camel/Templates.java
@@ -1,66 +1,67 @@
 package ch.dulce.camel;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.apache.camel.support.builder.Namespaces;
 
-import jakarta.enterprise.context.ApplicationScoped;
-
 @ApplicationScoped
-public class Templates extends RouteBuilder {
+public class Templates extends EndpointRouteBuilder {
 
-    private static final String MESSAGE = """
-            <object>
-              <header>
-                <system>${header.system}</system>
-                <serviceid>${header.serviceid}</serviceid>
-              </header>
-              <body>${body}</body>
-            </object>
-            """;
+  private static final String CACHE_LEVEL_NAME = "CACHE_CONSUMER";
 
-    @Override
-    public void configure() throws Exception {
+  private static final String MESSAGE = """
+      <object>
+        <header>
+          <system>${header.system}</system>
+          <serviceid>${header.serviceid}</serviceid>
+        </header>
+        <body>${body}</body>
+      </object>
+      """;
 
-        routeTemplate("wrapbodyTransformation")
-                .templateParameter("inqueue")
-                .templateParameter("outqueue")
-                .templateParameter("maxConsumers", "5")
-                .from("""
-                        jms:{{inqueue}}?maxConcurrentConsumers={{maxConsumers}}
-                        &lazyCreateTransactionManager=false
-                        &transacted=true
-                        &cacheLevelName=CACHE_CONSUMER""")
-                .transform().simple(MESSAGE)
-                .log(LoggingLevel.DEBUG, "wrapbodyTransformation", "${body}")
-                .to("jms:{{outqueue}}");
+  @Override
+  public void configure() throws Exception {
 
-        routeTemplate("uppercaseTransformation")
-                .templateParameter("inqueue")
-                .templateParameter("outqueue")
-                .templateParameter("maxConsumers", "5")
-                .from("""
-                        jms:{{inqueue}}?maxConcurrentConsumers={{maxConsumers}}
-                        &lazyCreateTransactionManager=false
-                        &transacted=true
-                        &cacheLevelName=CACHE_CONSUMER""")
-                .transform().simple("${uppercase()}")
-                .log(LoggingLevel.DEBUG, "uppercaseTransformation", "${body}")
-                .to("jms:{{outqueue}}");
+    routeTemplate("wrapbodyTransformation")
+        .templateParameter("inqueue")
+        .templateParameter("outqueue")
+        .templateParameter("maxConsumers", "5")
+        .from(jms("{{inqueue}}")
+          .transacted(true)
+          .cacheLevelName(CACHE_LEVEL_NAME)
+          .maxConcurrentConsumers("{{maxConsumers}}")
+          .advanced().lazyCreateTransactionManager(false))
+        .transform().simple(MESSAGE)
+        .log(LoggingLevel.DEBUG, "wrapbodyTransformation", "${body}")
+        .to("jms:{{outqueue}}");
 
-        routeTemplate("extractBodyTransformation")
-                .templateParameter("inqueue")
-                .templateParameter("outqueue")
-                .templateParameter("splitExpr")
-                .templateParameter("maxConsumers", "5")
-                .from("""
-                        jms:{{inqueue}}?maxConcurrentConsumers={{maxConsumers}}
-                        &lazyCreateTransactionManager=false
-                        &transacted=true
-                        &cacheLevelName=CACHE_CONSUMER""")
-                .split().xtokenize("{{splitExpr}}", 'u', new Namespaces())
-                .log(LoggingLevel.DEBUG, "extractBodyTransformation", "${body}")
-                .to("jms:{{outqueue}}");
-    }
+    routeTemplate("uppercaseTransformation")
+        .templateParameter("inqueue")
+        .templateParameter("outqueue")
+        .templateParameter("maxConsumers", "5")
+        .from(jms("{{inqueue}}")
+            .transacted(true)
+            .cacheLevelName(CACHE_LEVEL_NAME)
+            .maxConcurrentConsumers("{{maxConsumers}}")
+            .advanced().lazyCreateTransactionManager(false))
+        .transform().simple("${uppercase()}")
+        .log(LoggingLevel.DEBUG, "uppercaseTransformation", "${body}")
+        .to("jms:{{outqueue}}");
+
+    routeTemplate("extractBodyTransformation")
+        .templateParameter("inqueue")
+        .templateParameter("outqueue")
+        .templateParameter("splitExpr")
+        .templateParameter("maxConsumers", "5")
+        .from(jms("{{inqueue}}")
+            .transacted(true)
+            .cacheLevelName(CACHE_LEVEL_NAME)
+            .maxConcurrentConsumers("{{maxConsumers}}")
+            .advanced().lazyCreateTransactionManager(false))
+        .split().xtokenize("{{splitExpr}}", 'u', new Namespaces())
+        .log(LoggingLevel.DEBUG, "extractBodyTransformation", "${body}")
+        .to("jms:{{outqueue}}");
+  }
 
 }
