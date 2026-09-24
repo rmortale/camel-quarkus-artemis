c398e4e8eba4a06d84bb0aac4e88ab33202f3662
rmortale <rmortale@dulce.ch>
2026-09-24 17:35:45 +0200

    added templates

--- src/main/java/ch/dulce/camel/CamelRoutes.java
+++ src/main/java/ch/dulce/camel/CamelRoutes.java
@@ -7,21 +7,13 @@ import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 @ApplicationScoped
 public class CamelRoutes extends EndpointRouteBuilder {
 
+
+
   @Override
   public void configure() throws Exception {
-
-//    from(jms("inqueue")
-//        .transacted(true)
-//        .cacheLevelName("CACHE_CONSUMER")
-//        .maxConcurrentConsumers(5)
-//        .advanced().lazyCreateTransactionManager(false))
-//        .log("${body}");
-
-
-    // templatedRoute("extractBodyTransformation")
-    // .parameter("inqueue", "extractin")
-    // .parameter("outqueue", "extractout")
-    // .parameter("splitExpr", "/company/address");
   }
+
+
+
 
 }
